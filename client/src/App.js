ca5f02f9da387cd67a59481e5700e683402ebd85
weilyuwang <weilyuwang@gmail.com>
2020-11-06 15:56:25 -0500

    :fire: client code and logo

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src={logo}
+        alt="spacex"
+        style={{ width: 300, display: "block", margin: "auto" }}
+      />
+    </div>
+  );
+}
+
+export default App;
